baddfc37fb7eb2e53e69b3e843c0e4150c56d013
Steven G. Harms <steven@devbootcamp.com>
2014-03-12 14:58:52 -0700

    Show that `Model.new` allows default vars to be set

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,7 @@ class CategoriesController < ApplicationController
   # GET /categories/new.json
   def new
     @category = Category.new
+    @category.title = "snipple" if String(@category.title).empty?
 
     respond_to do |format|
       format.html # new.html.erb
